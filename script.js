f121cd29617c7877dd3f92dae8814e6320c786e5
Travis <tzito93@gmail.com>
2022-02-07 13:05:00 -0600

    Dividing by 0 throws error and prevents all button presses except for clear

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function multiplyNums(num1, num2) {
 }
 
 function divideNums(num1, num2) {
+    if (num2 == 0) {
+        return "ERROR: DIVIDE BY 0"
+    }
     return num1 / num2;
 }
 
@@ -30,6 +33,9 @@ function operate(operator, num1, num2) {
             val = divideNums(num1, num2);
             break;
     }
+    if (val == "ERROR: DIVIDE BY 0") {
+        return val;
+    }
     return val.toFixed(4);
 }
 
@@ -37,7 +43,9 @@ function operate(operator, num1, num2) {
 function initNumButtonEventListeners() {
     const buttons = document.querySelectorAll('button.num');
     buttons.forEach(button => button.addEventListener('click', () => {
-        display.textContent += button.textContent;
+        if (noError()) {
+            display.textContent += button.textContent;
+        }
     }));
 }
 
@@ -45,13 +53,15 @@ function initNumButtonEventListeners() {
 function initOperatorButtonEventListeners() {
     const buttons = document.querySelectorAll('button.operator');
     buttons.forEach(button => button.addEventListener('click', () => {
-        if (storedValue) {  // update the stored value first if there already is one
-            storedValue = operate(lastOperatorEntered, +storedValue, +display.textContent);
-        }   else {            
-            storedValue = display.textContent;
-        }
-        lastOperatorEntered = button.textContent;        
-        display.textContent = "";
+        if (noError()) {
+            if (storedValue) {  // update the stored value first if there already is one
+                storedValue = operate(lastOperatorEntered, +storedValue, +display.textContent);
+            }   else {            
+                storedValue = display.textContent;
+            }
+            lastOperatorEntered = button.textContent;        
+            display.textContent = "";
+        }        
     }));
 }
 
@@ -60,11 +70,13 @@ function initOperatorButtonEventListeners() {
 function initEqualsButtonEventListener() {
     const button = document.querySelector('button.equals');
     button.addEventListener('click', () => {
-        if (storedValue && lastOperatorEntered) {
-            display.textContent = operate(lastOperatorEntered, +storedValue, +display.textContent);
-            storedValue = null;
-            lastOperatorEntered = null;          
-        }
+        if (noError()) {
+            if (storedValue && lastOperatorEntered) {
+                display.textContent = operate(lastOperatorEntered, +storedValue, +display.textContent);
+                storedValue = null;
+                lastOperatorEntered = null;          
+            }
+        }        
     })
 }
 
@@ -75,6 +87,13 @@ function initClearButtonEventListener() {
         storedValue = null;
         lastOperatorEntered = null;
     })
+}
+
+function noError() {
+    if (display.textContent == "ERROR: DIVIDE BY 0") {
+        return false;
+    }
+    return true;
 }
 
 function initButtonEventListeners() {
